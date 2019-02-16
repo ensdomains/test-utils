ae70dc14d08c547de1b3f43d0fb555ba38bfafc8
decanus <dean.eigenmann@icloud.com>
2019-02-16 14:39:04 +0100

    adds expectFailure function

--- src/exception.js
+++ src/exception.js
@@ -7,6 +7,23 @@ function ensureException(error) {
     assert(isException(error), error.toString());
 }
 
+async function expectFailure(call) {
+	let tx;
+	try {
+		tx = await call;
+	} catch (error) {
+		// Assert ganache revert exception
+		assert.equal(
+			error.message,
+			'Returned error: VM Exception while processing transaction: revert'
+		);
+	}
+	if(tx !== undefined) {
+		assert.equal(parseInt(tx.receipt.status), 0);
+	}
+}
+
 module.exports = {
     ensureException: ensureException,
+    expectFailure: expectFailure
 }
